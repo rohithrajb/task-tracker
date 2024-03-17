d74051b5b44a241f408a0b9ab7720e51eb3e76c8
Rohith Raj Bingi <rohithrajb@gmail.com>
2024-03-18 02:13:30 +0530

    Angular Material DatePicker added but not working as input yet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,37 +18,44 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MaterialDatepickerComponent } from './components/material-datepicker/material-datepicker.component';
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: TasksComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
-    }
-]
+	{
+		path: '',
+		component: TasksComponent,
+	},
+	{
+		path: 'about',
+		component: AboutComponent,
+	},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ButtonComponent,
-    TasksComponent,
-    TaskItemComponent,
-    AddTaskComponent,
-    AboutComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule,
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		ButtonComponent,
+		TasksComponent,
+		TaskItemComponent,
+		AddTaskComponent,
+		AboutComponent,
+		FooterComponent,
+		MaterialDatepickerComponent,
+	],
+	imports: [
+		BrowserModule,
+		FontAwesomeModule,
+		HttpClientModule,
+		FormsModule,
+		RouterModule.forRoot(appRoutes),
+		MatDatepickerModule,
+		MatFormFieldModule,
+		MatInputModule,
+		BrowserAnimationsModule,
+		MatNativeDateModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/components/material-datepicker/material-datepicker.component.ts
@@ -0,0 +1,14 @@
+import { Component, Output } from '@angular/core';
+
+@Component({
+	selector: 'app-material-datepicker',
+	templateUrl: './material-datepicker.component.html',
+	styleUrls: ['./material-datepicker.component.css'],
+})
+export class MaterialDatepickerComponent {
+	
+	date?: string;
+
+    // @Output()
+
+}
